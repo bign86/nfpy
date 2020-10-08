e84f3df5600079cfe3b614cba6455c487dd6dda4
bign86 <72072987+bign86@users.noreply.github.com>
2020-10-08 10:22:45 +0200

    Update RateFactory.py
    Added initialization to rate factory

--- nfpy/Handlers/RateFactory.py
+++ nfpy/Handlers/RateFactory.py
@@ -18,23 +18,30 @@ class RateFactory(metaclass=Singleton):
 
     _CURVE_TABLE = 'Curve'
     _RATE_TABLE = 'Rate'
-    _Q_GET_RF = """select r.uid from Rate as r join CurveConstituents as cc
-on r.uid = cc.bucket join Curve as c on c.uid = cc.uid
-where c.currency = ? and r.is_rf = True;"""
+    _Q_GET_RF = """select c.currency, r.uid from Rate as r join CurveConstituents as cc
+on r.uid = cc.bucket join Curve as c on c.uid = cc.uid where r.is_rf = True;"""
     _Q_SET_RF = """update Rate set is_rf = ? where uid = ?;"""
 
     def __init__(self):
         self._db = get_db_glob()
         self._qb = get_qb_glob()
         self._af = get_af_glob()
+        self._rf = {}
+        
+        self._initialize()
+    
+    def _initialize():
+        res = self._db.execute(self._Q_GET_RF).fetchall()
+        if not res:
+            raise MissingData('No risk free found in the database')
+        self._rf = {(t[0], t[1]) for t in res}
 
     def get_rf(self, ccy: str) -> Rate:
-        res = self._db.execute(self._Q_GET_RF, (ccy,)).fetchall()
-        if not res:
-            raise MissingData('No risk free found for {}'.format(ccy))
-        if len(res) > 1:
-            raise ValueError('More than one risk free found for {}'.format(ccy))
-        return self._af.get(res[0][0])
+        try:
+            uid = self._rf[ccy]
+        except KeyError:
+            raise DataMissing('No risk free set for currency {}'.format(ccy))
+        return self._af.get(uid)
 
     def set_rf(self, ccy: str, rate: str):
         """ Set a risk free rate. """
