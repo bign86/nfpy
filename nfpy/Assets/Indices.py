afd6d8616550ba2e2f75d6b9148c11a2929d1bfd
AN <hellnote01-github@yahoo.com>
2020-11-23 17:08:30 +0100

    Cleaned up the code

--- nfpy/Assets/Indices.py
+++ nfpy/Assets/Indices.py
@@ -3,7 +3,6 @@
 # Class for indices
 #
 
-import pandas as pd
 from nfpy.Assets.Asset import Asset
 
 
@@ -14,12 +13,3 @@ class Indices(Asset):
     _BASE_TABLE = 'Indices'
     _TS_TABLE = 'IndexTS'
     _TS_ROLL_KEY_LIST = ['date']
-
-    @property
-    def prices(self) -> pd.Series:
-        try:
-            res = self._df["price"]
-        except KeyError:
-            self.load_dtype("price")
-            res = self._df["price"]
-        return res
