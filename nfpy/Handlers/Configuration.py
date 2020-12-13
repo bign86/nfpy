55728f8ab55e9c0fef07fcb169d3641262c14d2c
AN <hellnote01-github@yahoo.com>
2020-12-13 11:43:17 +0100

    Removed explicit properties, adjusted dictionary accessors

--- nfpy/Handlers/Configuration.py
+++ nfpy/Handlers/Configuration.py
@@ -45,7 +45,6 @@ PARAMS_DICT__ = {'dbf': 'database folder', 'dbn': 'database name',
                  'ib_intfc': 'IBAPI interface', 'ib_client': 'IBAPI client ID',
                  'ib_port': 'IBAPI TWS port', 'zip_format': 'Archive format',
                  'rep_arch_path': 'Path to archive directory',
-                 'rep_arch_dir': 'Archive directory',
                  'rep_ret': 'Report retention days',
                  'rep_path': 'Path to report directory'}
 
@@ -68,72 +67,12 @@ class Configuration(metaclass=Singleton):
 
     def __getitem__(self, k: str) -> str:
         try:
-            return self._conf[k]
+            return getattr(self, k, None)
         except KeyError:
             raise KeyError(k, 'Key {} does not exists in configuration file'.format(k))
 
     def __setitem__(self, k: str, v: Any):
-        self._conf[k] = v
-
-    @property
-    def conf(self) -> configparser.ConfigParser:
-        return self._conf
-
-    @property
-    def db_path(self) -> str:
-        return self._conf.get('DATABASE', 'db_path')
-
-    @property
-    def db_dir(self) -> str:
-        return self._conf.get('DATABASE', 'db_dir')
-
-    @property
-    def backup_dir(self) -> str:
-        return self._conf.get('FOLDERS', 'backup_folder')
-
-    @property
-    def data_dir(self) -> str:
-        return self._conf.get('FOLDERS', 'data_folder')
-
-    @property
-    def base_ccy(self) -> str:
-        return self._conf.get('OTHERS', 'base_ccy')
-
-    @property
-    def frequency(self) -> str:
-        return self._conf.get('OTHERS', 'calendar_frequency')
-
-    @property
-    def ib_interface(self) -> str:
-        return self._conf.get('IBAPI', 'ib_interface')
-
-    @property
-    def ib_client_id(self) -> int:
-        return self._conf.getint('IBAPI', 'ib_client_id')
-
-    @property
-    def ib_tws_port(self) -> int:
-        return self._conf.getint('IBAPI', 'ib_tws_port')
-
-    @property
-    def zip_format(self) -> str:
-        return self._conf.get('REPORTING', 'zip_format')
-
-    @property
-    def report_path(self) -> str:
-        return self._conf.get('REPORTING', 'report_path')
-
-    @property
-    def report_arch_path(self) -> str:
-        return self._conf.get('REPORTING', 'report_arch_path')
-
-    @property
-    def report_arch_dir(self) -> str:
-        return self._conf.get('REPORTING', 'report_arch_dir')
-
-    @property
-    def report_retention(self) -> int:
-        return self._conf.getint('REPORTING', 'report_retention')
+        setattr(self, k, v)
 
     def _get_conf_full_path(self) -> str:
         """ Return the full path of the nfpyConf.ini file by interrogating the
@@ -141,7 +80,6 @@ class Configuration(metaclass=Singleton):
             FIXME: The position must be standardized possibly in a configuration
                    folder somewhere in the user home.
         """
-        # p = dirname(dirname(abspath(__file__)))
         return os.path.join(NFPY_ROOT_DIR, self.CONF_INI)
 
     def _parse(self):
@@ -156,9 +94,10 @@ class Configuration(metaclass=Singleton):
         db_path = os.path.join(config['DATABASE']['db_dir'], config['DATABASE']['db_name'])
         config['DATABASE']['db_path'] = db_path
 
-        # fx = config['OTHERS']['base_ccy']
-        # if fx not in BASE_CCY:
-        #    raise ValueError('Base currency in nfpyConf.ini not valid')
+        for section in config.values():
+            for k, v in section.items():
+                print(k, v)
+                setattr(self, k, v)
 
         self._conf_path = conf_path
         self._conf = config
