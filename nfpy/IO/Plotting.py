1c2e7643ec7753e9650eb0a1c17058a07c3a2638
AN <hellnote01-github@yahoo.com>
2021-02-19 22:20:30 +0100

    Add shiftedColorMap() function

--- nfpy/IO/Plotting.py
+++ nfpy/IO/Plotting.py
@@ -4,12 +4,16 @@
 #
 
 from abc import ABCMeta
+import matplotlib
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import AxesGrid
 import numpy as np
 import pandas as pd
 from typing import (Union, Sequence)
 
-from nfpy.Financial.Optimizer import OptimizerResult
+from nfpy.Financial import OptimizerResult
+
+
 
 plt.style.use('seaborn')
 # print(plt.style.library['seaborn'])
@@ -282,3 +286,58 @@ class PlotPortfolioOptimization(PlotVarRet):
         self._plots = plots
 
         super().plot()
+
+
+def shiftedColorMap(cmap, start=0, midpoint=0.5, stop=1.0, name='shiftedcmap'):
+    '''
+    Function to offset the "center" of a colormap. Useful for
+    data with a negative min and positive max and you want the
+    middle of the colormap's dynamic range to be at zero.
+
+    Input
+    -----
+      cmap : The matplotlib colormap to be altered
+      start : Offset from lowest point in the colormap's range.
+          Defaults to 0.0 (no lower offset). Should be between
+          0.0 and `midpoint`.
+      midpoint : The new center of the colormap. Defaults to 
+          0.5 (no shift). Should be between 0.0 and 1.0. In
+          general, this should be  1 - vmax / (vmax + abs(vmin))
+          For example if your data range from -15.0 to +5.0 and
+          you want the center of the colormap at 0.0, `midpoint`
+          should be set to  1 - 5/(5 + 15)) or 0.75
+      stop : Offset from highest point in the colormap's range.
+          Defaults to 1.0 (no upper offset). Should be between
+          `midpoint` and 1.0.
+    '''
+    cdict = {
+        'red': [],
+        'green': [],
+        'blue': [],
+        'alpha': []
+    }
+
+    cmap_obj = getattr(matplotlib.cm, cmap)
+
+    # regular index to compute the colors
+    reg_index = np.linspace(start, stop, 257)
+    print(reg_index[0], reg_index[128], reg_index[-1])
+
+    # shifted index to match the data
+    shift_index = np.hstack([
+        np.linspace(0.0, midpoint, 128, endpoint=False),
+        np.linspace(midpoint, 1.0, 129, endpoint=True)
+    ])
+
+    for ri, si in zip(reg_index, shift_index):
+        r, g, b, a = cmap_obj(ri)
+
+        cdict['red'].append((si, r, r))
+        cdict['green'].append((si, g, g))
+        cdict['blue'].append((si, b, b))
+        cdict['alpha'].append((si, a, a))
+
+    newcmap = matplotlib.colors.LinearSegmentedColormap(name, cdict)
+    plt.register_cmap(cmap=newcmap)
+
+    return newcmap
--- nfpy/IO/__init__.py
+++ nfpy/IO/__init__.py
@@ -1,9 +1,11 @@
 from .Inputs import (InputHandler, SQLITE2PY_CONVERSION)
 from .Plotting import (PlotLine, PlotTS, PlotBeta,
-                       PlotVarRet, PlotPortfolioOptimization)
+                       PlotVarRet, PlotPortfolioOptimization,
+                       shiftedColorMap)
 from .Reporting import *
 
 __all__ = [
     'get_re_glob', 'InputHandler', 'SQLITE2PY_CONVERSION', 'PlotLine',
-    'PlotTS', 'PlotBeta', 'PlotVarRet', 'PlotPortfolioOptimization'
+    'PlotTS', 'PlotBeta', 'PlotVarRet', 'PlotPortfolioOptimization',
+    'shiftedColorMap'
 ]
